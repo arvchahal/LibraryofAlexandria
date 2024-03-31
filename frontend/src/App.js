b34e8e3f35e316a17e1e86b9970c1c37d8358cc9
arnavchahal <arnav.chahal@vanderbilt.edu>
2024-03-30 23:04:53 -0500

    Adding page styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,53 @@
+import React from 'react';
 import './App.css';
-import { ImBooks } from "react-icons/im"; // users librabry with books they have read
+import { ImBooks } from "react-icons/im"; // users library with books they have read
 import { CiSearch } from "react-icons/ci"; //search icon for searching different books
 import { HiBuildingLibrary } from "react-icons/hi2"; //library icon returns to the homepage
-import { IoIosThumbsUp } from "react-icons/io"; //thumbs up for the reccomendations for the user
-
+import { IoIosThumbsUp } from "react-icons/io"; //thumbs up for the recommendations for the user
+import { IconContext } from 'react-icons';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <HiBuildingLibrary size = "30px"/>
-      <CiSearch size = "30px"/>
-      <ImBooks size = "30px"/>
-      <IoIosThumbsUp size = "30px"/>
+    <IconContext.Provider value={{ size: "30px", color:"#FA7202" }}>
+      <div className="App">
+        <div className="iconWithText">
+          <Link to="/Main" className="Home"> 
+            <HiBuildingLibrary className='libraryicon'/>
+            <span>Home</span>
+          </Link>
+        </div>
+        <div className="iconWithText">
+          <Link to ="/Search" className='Search'>
+            <CiSearch/>
+            <span>Search</span>
+          </Link>
+        </div>
 
-    </div>
+
+        <div className="iconWithText">
+          <Link to ="/Library" className = "Your_Library">
+          <ImBooks/>
+          <span>Your Books</span>
+          </Link>
+        </div>
+
+        <div className="iconWithText">
+          <Link to ="/Reccomendations" className ="Reccs">
+          <IoIosThumbsUp/>
+          <span>Recommendations</span>
+          </Link>
+        </div>
+
+        <div className='iconWithText'>
+        <Link to ="/Register" className ="SignIn">
+          
+          <span>Log In</span>
+          </Link>
+
+        </div>
+      </div>
+    </IconContext.Provider>
   );
 }
 
--- frontend/src/homePage.js
+++ frontend/src/homePage.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import backgroundImage from "./components/nigtlibrary.webp";
+//import backgroundImage from "./components/nigtlibrary.webp";
 import { Link } from 'react-router-dom';
+import "./homePage.css"
 
 const HomePage = () => {
   return (
-    <div className = "image" style={{
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      margin: 'auto', // Ensure no margin
-      display: 'block', // Use flex to center the content
-      flexDirection: 'column', // Stack children vertically
-      justifyContent: 'center', // Center content vertically
-    }}>
+
       <header className="header">
         <h1>Welcome to The Library of Alexandria</h1>
         <Link to="/Main" className="Enter">Press to Enter</Link>
       </header>
-      
-    </div>
+    
   );
 }
 
