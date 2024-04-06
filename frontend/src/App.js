7334f1ed7a4f877294225604fe9bffc4b36e532a
arnavchahal <arnav.chahal@vanderbilt.edu>
2024-04-05 21:27:50 -0500

    Fixing styling adding section headers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import { ImBooks } from "react-icons/im"; // users library with books they have read
 import { CiSearch } from "react-icons/ci"; //search icon for searching different books
 import { HiBuildingLibrary } from "react-icons/hi2"; //library icon returns to the homepage
-import { IoIosThumbsUp } from "react-icons/io"; //thumbs up for the recommendations for the user
 import { IconContext } from 'react-icons';
 import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <IconContext.Provider value={{ size: "30px", color:"#FA7202" }}>
+    
+    <IconContext.Provider value={{ size: "30px", color:"#90bdc9" }}>
       <div className="App">
+        <div className='homebar'>
         <div className="iconWithText">
           <Link to="/Main" className="Home"> 
             <HiBuildingLibrary className='libraryicon'/>
@@ -27,14 +28,14 @@ function App() {
 
         <div className="iconWithText">
           <Link to ="/Library" className = "Your_Library">
-          <ImBooks/>
+          <ImBooks className = 'bookicon'/>
           <span>Your Books</span>
           </Link>
         </div>
 
         <div className="iconWithText">
           <Link to ="/Reccomendations" className ="Reccs">
-          <IoIosThumbsUp/>
+          
           <span>Recommendations</span>
           </Link>
         </div>
@@ -46,6 +47,32 @@ function App() {
           </Link>
 
         </div>
+        </div>
+
+        {/* Latest Releases Section */}
+        <div className='section'>
+          <div className='section-title'>Latest Releases</div>
+          <div className='book-grid'>
+  
+          </div>
+        </div>
+
+        {/* NYT Best Sellers Section */}
+        <div className='section'>
+          <div className='section-title'>NYT Best Sellers</div>
+          <div className='book-grid'>
+
+          </div>
+        </div>
+
+        {/* Timeless Classics Section */}
+        <div className='section'>
+          <div className='section-title'>Timeless Classics</div>
+          <div className='book-grid'>
+
+          </div>
+        </div>
+        
       </div>
     </IconContext.Provider>
   );
