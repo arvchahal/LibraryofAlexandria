1db555fa25cc9c29a6424a98f24c5ab00f905b48
arnavchahal <arnav.chahal@vanderbilt.edu>
2024-05-05 22:40:17 -0400

    django skeletons

---
+++ backend/getInfo.py
@@ -0,0 +1,19 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import json
+
+cred = credentials.Certificate('libofalex-8397c-firebase-adminsdk-v45ws-38bb278209.json')
+
+firebase_admin.initialize_app(cred,{
+
+  'databaseURL' : 'https://libofalex-8397c-default-rtdb.firebaseio.com/'
+})
+
+ref = db.reference("/")
+ref = ref.get()
+ref = json.dumps(ref,indent=4)
+
+with open('tst.json','w') as f:
+  f.write(ref)
+print('done')
